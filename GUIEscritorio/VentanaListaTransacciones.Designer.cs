d1b5b052bdef907f5513ffc22d366920f8d928c3
se.groselj <se.groselj@alumnos.duoc.cl>
2020-10-16 02:08:22 -0300

    Ventana Lista Transacciones

--- GUIEscritorio/VentanaListaTransacciones.Designer.cs
+++ GUIEscritorio/VentanaListaTransacciones.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btn_Salir = new System.Windows.Forms.Button();
+            this.DataGridView_Tabla = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Salir
+            // 
+            this.Btn_Salir.Location = new System.Drawing.Point(499, 415);
+            this.Btn_Salir.Name = "Btn_Salir";
+            this.Btn_Salir.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Salir.TabIndex = 0;
+            this.Btn_Salir.Text = "Salir";
+            this.Btn_Salir.UseVisualStyleBackColor = true;
+            this.Btn_Salir.Click += new System.EventHandler(this.Btn_Salir_Click);
+            // 
+            // DataGridView_Tabla
+            // 
+            this.DataGridView_Tabla.AllowUserToAddRows = false;
+            this.DataGridView_Tabla.AllowUserToDeleteRows = false;
+            this.DataGridView_Tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView_Tabla.Location = new System.Drawing.Point(12, 12);
+            this.DataGridView_Tabla.Name = "DataGridView_Tabla";
+            this.DataGridView_Tabla.Size = new System.Drawing.Size(562, 397);
+            this.DataGridView_Tabla.TabIndex = 1;
+            // 
+            // VentanaListaTransacciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "VentanaListaTransacciones";
+            this.ClientSize = new System.Drawing.Size(590, 450);
+            this.Controls.Add(this.DataGridView_Tabla);
+            this.Controls.Add(this.Btn_Salir);
+            this.Name = "VentanaListaTransacciones";
+            this.Text = "Lista de Transacciones de Los Tres Chanchitos";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Tabla)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_Salir;
+        private System.Windows.Forms.DataGridView DataGridView_Tabla;
     }
 }
--- GUIEscritorio/VentanaListaTransacciones.cs
+++ GUIEscritorio/VentanaListaTransacciones.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Negocio;
+using System.Data;
+using ModeloDeDatos;
 
 namespace GUIEscritorio
 {
     public partial class VentanaListaTransacciones : Form
     {
+        private NegocioTransaccion nTransaccion;
+        private DataTable tablaTransaccion;
+
+        public NegocioTransaccion NTransaccion { get => nTransaccion; set => nTransaccion = value; }
+        public DataTable TablaTransaccion { get => tablaTransaccion; set => tablaTransaccion = value; }
+
         public VentanaListaTransacciones()
         {
             InitializeComponent();
+            ActualizarTabla();
+        }
+
+        private void Btn_Salir_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            System.GC.Collect();
+            this.Close();
+        }
+
+        private void ActualizarTabla()
+        {
+            NTransaccion = new NegocioTransaccion();
+            TablaTransaccion = NTransaccion.GetAll();
+
+            DataGridView_Tabla.DataSource = TablaTransaccion;
+
+            DataGridView_Tabla.Columns["rut_paciente"].HeaderText = "RUT Paciente";
+            DataGridView_Tabla.Columns["nom_paciente"].HeaderText = "Nombre Paciente";
+            DataGridView_Tabla.Columns["estatura"].HeaderText = "Estatura";
+            DataGridView_Tabla.Columns["peso"].HeaderText = "Peso";
+            DataGridView_Tabla.Columns["IMC"].HeaderText = "IMC";
+            DataGridView_Tabla.Columns["estado"].HeaderText = "Estado";
         }
     }
 }
