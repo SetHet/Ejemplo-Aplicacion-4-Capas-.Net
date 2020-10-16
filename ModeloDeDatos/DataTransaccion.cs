8078cd9f626f6344fbff0cefed57a3eb2e2e36bc
se.groselj <se.groselj@alumnos.duoc.cl>
2020-10-16 00:16:10 -0300

    Modelo de datos y Negocio

---
+++ ModeloDeDatos/DataTransaccion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDeDatos
+{
+    public class DataTransaccion
+    {
+        private string rut_paciente;
+        private string nom_paciente;
+        private float estatura;
+        private int peso;
+        private float imc;
+        private string estado;
+
+        private const int rut_paciente_maxLenght = 15;
+        private const int nombre_paciente_maxLenght = 40;
+        private const int estado_maxLenght = 15;
+
+        public string Rut_paciente { get => rut_paciente; set => rut_paciente = value; }
+        public string Nom_paciente { get => nom_paciente; set => nom_paciente = value; }
+        public float Estatura { get => estatura; set => estatura = value; }
+        public int Peso { get => peso; set => peso = value; }
+        public float Imc { get => imc; set => imc = value; }
+        public string Estado { get => estado; set => estado = value; }
+
+        public static int Rut_paciente_maxLenght => rut_paciente_maxLenght;
+
+        public static int Nombre_paciente_maxLenght => nombre_paciente_maxLenght;
+
+        public static int Estado_maxLenght => estado_maxLenght;
+    }
+}
---
+++ Negocio/NegocioTransaccion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using ConexionBaseDeDatos;
+using ModeloDeDatos;
+
+namespace Negocio
+{
+    public class NegocioTransaccion
+    {
+        private DataTransaccion dTransaccion;
+        private DataTable tableTransacciones;
+        private ConexionSQLServer conexion;
+
+        public DataTransaccion DTransaccion { get => dTransaccion; set => dTransaccion = value; }
+        public DataTable TableTransacciones { get => tableTransacciones; private set => tableTransacciones = value; }
+        private ConexionSQLServer Conexion { get => conexion; set => conexion = value; }
+
+        public NegocioTransaccion()
+        {
+            Conexion = new ConexionSQLServer();
+            Conexion.Server = "DESKTOP-GBD1PJB";
+            Conexion.DataBase = "TEST";
+            Conexion.Conectar();
+            NewTransaccion();
+            TableTransacciones = new DataTable();
+        }
+
+        public DataTable GetAll()
+        {
+            string query = "SELECT * FROM transaccion";
+            TableTransacciones = Conexion.Select(query);
+            return TableTransacciones;
+        }
+
+        public bool Save()
+        {
+            string query = $"INSERT INTO transaccion (rut_paciente, nom_paciente, estatura, peso, IMC, estado) VALUES " +
+                $"('{DTransaccion.Rut_paciente}', '{DTransaccion.Nom_paciente}', {DTransaccion.Estatura}, {DTransaccion.Peso}, {DTransaccion.Imc}, '{DTransaccion.Estado}')";
+
+            bool correcto = conexion.NonQuery(query);
+
+            return correcto;
+        }
+
+        public void NewTransaccion()
+        {
+            DTransaccion = new DataTransaccion();
+        }
+
+    }
+}
